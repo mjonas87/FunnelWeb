84bc7038826f9c088c0b96fe60c98a3ba4e1e16f
Matt Jonas <mjonas@nerdery.com>
2013-11-12 22:36:12 -0600

    Changes

---
+++ src/FunnelWeb/Settings/ConfigurationManager.cs
@@ -0,0 +1,32 @@
+﻿#region
+
+using System.Configuration;
+using System.Diagnostics.Contracts;
+
+#endregion
+
+namespace FunnelWeb.Settings
+{
+    public class CustomConfigurationManager : IConfigurationManager
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                var connection = ConfigurationManager.ConnectionStrings["MyDb"];
+                Contract.Assume(connection != null);
+                return connection.ConnectionString;
+            }
+        }
+
+        public void AppSettings(string key, string value)
+        {
+            ConfigurationManager.AppSettings[key] = value;
+        }
+
+        public string AppSettings(string key)
+        {
+            return ConfigurationManager.AppSettings[key];
+        }
+    }
+}
---
+++ src/FunnelWeb/Settings/IConfigurationManager.cs
@@ -0,0 +1,14 @@
+﻿#region
+
+#endregion
+
+namespace FunnelWeb.Settings
+{
+    public interface IConfigurationManager
+    {
+        string ConnectionString { get; }
+
+        void AppSettings(string key, string value);
+        string AppSettings(string key);
+    }
+}
